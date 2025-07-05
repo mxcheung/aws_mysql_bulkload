35bdc02a010dd9ec01242eedc6ab1547454636a4
mxcheung <mxcheung@users.noreply.github.com>
2025-07-05 21:56:52 +1000

    Update set_up.sh

--- set_up.sh
+++ set_up.sh
@@ -4,5 +4,5 @@
 cd /home/ec2-user/environment/aws_mysql_bulkload/make_data
 . ./set_up.sh
 
-cd /home/ec2-user/environment/aws_mysql_bulkload/spring_batch
-. ./set_up.sh
+# cd /home/ec2-user/environment/aws_mysql_bulkload/spring_batch
+# . ./set_up.sh
