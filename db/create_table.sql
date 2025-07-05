e247e05cff1ea25868a1946f0c9dc5a2880fd6b8
mxcheung <mxcheung@users.noreply.github.com>
2025-07-05 21:26:22 +1000

    Update create_table.sql

--- db/create_table.sql
+++ db/create_table.sql
@@ -4,3 +4,11 @@ CREATE TABLE users (
     name VARCHAR(100),
     email VARCHAR(100)
 );
+
+
+CREATE TABLE my_table (
+  id INT PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100),
+  created_at DATETIME
+);
