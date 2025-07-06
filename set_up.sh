f6fe0bc89f6e7e5273306a64f5dca0d5e8a7111d
max <maxcheung@optusnet.com.au>
2025-07-06 21:40:16 +1000

    update tables spring batch

--- set_up.sh
+++ set_up.sh
@@ -5,6 +5,9 @@ echo $MY_ENV_DIR
 cd $MY_ENV_DIR/aws_mysql_bulkload/make_data
 . ./set_up.sh
 
+cd $MY_ENV_DIR/aws_mysql_bulkload/create_tables
+. ./set_up.sh
+
 cd $MY_ENV_DIR/aws_mysql_bulkload/load_data
 . ./set_up.sh
 
